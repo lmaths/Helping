91362ba612aa614c2df3df00d7e9ae7e5bf6aa98
Matheus Lima <matheusldasilva20088@gmail.com>
2020-05-23 10:32:23 -0300

    adiciona atributos no model

--- app/src/main/java/com/rightside/helping/PrincipalActivity.java
+++ app/src/main/java/com/rightside/helping/PrincipalActivity.java
@@ -8,14 +8,20 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.rightside.helping.models.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private List<Pessoa> pessoaList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,16 @@ public class PrincipalActivity extends FragmentActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Pessoa pessoa = new Pessoa("joao", "1", -21.658840, -42.347540, "Donatario");
+        Pessoa joao = new Pessoa("Pedro", "2", -21.655753,-42.347941, "Donatario");
+        pessoaList = new ArrayList<>();
+
+        pessoaList.add(pessoa);
+        pessoaList.add(joao);
+
+
+
     }
 
 
@@ -32,5 +48,10 @@ public class PrincipalActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
+            for(Pessoa p : pessoaList) {
+                mMap.addMarker(new MarkerOptions().position(p.getLatLng()).title(p.getNome()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p.getLatLng(), 17f));
+            }
+
     }
 }
--- app/src/main/java/com/rightside/helping/models/Pessoa.java
+++ app/src/main/java/com/rightside/helping/models/Pessoa.java
@@ -4,11 +4,21 @@ import com.google.android.gms.maps.model.LatLng;
 
 import lombok.Data;
 
+
 @Data
 public class Pessoa {
     private String nome;
     private String id;
+    private String tipo = "";
     private double latitude, longitude;
+
+    public Pessoa(String nome, String id, double latitude, double longitude, String tipo) {
+        this.nome = nome;
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.tipo = tipo;
+    }
 
     public LatLng getLatLng() {
         return new LatLng(latitude, longitude);

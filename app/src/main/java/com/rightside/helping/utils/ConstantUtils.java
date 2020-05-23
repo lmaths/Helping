15aa53c3c82b9adefd9c77e172643d4f9b611ff5
Matheus Lima <matheusldasilva20088@gmail.com>
2020-05-23 12:34:53 -0300

    ajusta string

--- app/src/main/java/com/rightside/helping/utils/ConstantUtils.java
+++ app/src/main/java/com/rightside/helping/utils/ConstantUtils.java
@@ -3,6 +3,6 @@ package com.rightside.helping.utils;
 public class ConstantUtils {
     public static final String PESSOAS = "pessoas";
     public static final String DONATARIO = "DONATARIO";
-    public static final String EMPRESA = "DONATARIO";
+    public static final String EMPRESA = "EMPRESA";
     public static final String CONSUMIDOR = "CONSUMIDOR";
 }

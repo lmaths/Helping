173ff4127a53d769b1aa0b63f9fda0fd1d18d1c3
Ivan Viana <ivan.viana@live.com>
2020-05-23 15:14:16 -0300

    tela de produtos

---
+++ app/src/main/java/com/rightside/helping/adapter/ProdutoAdapter.java
@@ -0,0 +1,62 @@
+package com.rightside.helping.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rightside.helping.R;
+import com.rightside.helping.adapter.ProdutoAdapter.ProdutoViewHolder;
+import com.rightside.helping.models.Produto;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProdutoAdapter extends RecyclerView.Adapter<ProdutoViewHolder> {
+
+    List<Produto> produtos ;
+    Context context;
+
+    public ProdutoAdapter(List<Produto> produtos, Context context) {
+        this.produtos = produtos;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProdutoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_produto, parent, false);
+        return new ProdutoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProdutoViewHolder holder, int position) {
+        Produto p = produtos.get(position);
+
+        holder.textViewNome.setText(p.getNome());
+        holder.textViewPreco.setText(String.valueOf(p.getPreco()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return produtos.size();
+    }
+
+    public class ProdutoViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.textView_nome_produto) TextView textViewNome;
+        @BindView(R.id.textView_preco_produto) TextView textViewPreco;
+
+        public ProdutoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+        }
+    }
+}

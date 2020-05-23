09e0766bf2b6144a4ac51668bb17f0bd2ce3317f
Matheus Lima <matheusldasilva20088@gmail.com>
2020-05-23 14:21:00 -0300

    adiciona metodos para recuperar pessoa

--- app/src/main/java/com/rightside/helping/Repository/FirebaseRepository.java
+++ app/src/main/java/com/rightside/helping/Repository/FirebaseRepository.java
@@ -1,12 +1,21 @@
 package com.rightside.helping.Repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.rightside.helping.models.Pessoa;
 import com.rightside.helping.utils.ConstantUtils;
 
+import java.util.List;
+
 public class FirebaseRepository {
+
+    private MutableLiveData<List<Pessoa>> mutableLiveDataPessoas = new MutableLiveData<>();
 
     public static FirebaseFirestore getBanco() {
         return FirebaseFirestore.getInstance();
@@ -15,6 +24,29 @@ public class FirebaseRepository {
     public static Task<Void> salvarPessoa(final Pessoa user) {
         return getBanco().collection(ConstantUtils.PESSOAS).document(user.getId()).set(user.returnPessoa());
     }
+
+    public static String getIdPessoa() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    public static DocumentReference getPessoa(String id) {
+        return getBanco().collection(ConstantUtils.PESSOAS).document(id);
+    }
+
+    public static CollectionReference getPessoas() {
+        return getBanco().collection(ConstantUtils.PESSOAS);
+    }
+
+
+
+    public LiveData<List<Pessoa>> getMutableLiveDataPessoas() {
+        getPessoas().addSnapshotListener((queryDocumentSnapshots, e) -> {
+           mutableLiveDataPessoas.setValue(queryDocumentSnapshots.toObjects(Pessoa.class));
+        });
+        return mutableLiveDataPessoas;
+    }
+
+
 
 
 }

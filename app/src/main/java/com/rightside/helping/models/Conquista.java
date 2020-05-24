ed6e593e6e53348387a32946b7dd6d15decf0a5c
Matheus Lima <matheusldasilva20088@gmail.com>
2020-05-24 12:37:04 -0300

    adiciona conquista

---
+++ app/src/main/java/com/rightside/helping/models/Conquista.java
@@ -0,0 +1,34 @@
+package com.rightside.helping.models;
+
+import androidx.annotation.StringDef;
+
+public class Conquista {
+    private String nome;
+    private String descricao;
+    private int xp;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+}
